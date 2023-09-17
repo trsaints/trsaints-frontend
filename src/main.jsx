7b9dd83a73ed531380bcede9c1a338d72badce53
trsaints <thiago.dev2k3@gmail.com>
2023-09-17 08:49:46 -0300

    Add css reset/normalizer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/components/App/App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/components/App/App";
+import "./reset.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
