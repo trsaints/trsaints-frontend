becbe89458e009ec32fbdfbe21e8af7dac86ce2f
trsaints <thiago.dev2k3@gmail.com>
2024-01-17 10:57:11 -0300

    mockup structure for future api implementation - II

--- src/app/services/db-service.js
+++ src/app/services/db-service.js
@@ -6,10 +6,27 @@ function getAllSkills() {
   return ["skill issues", "more skill issues"];
 }
 
+function getAllSocialLinks() {
+  return [
+    {
+      name: "Github",
+      url: "https://github.com/trsaints",
+    },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/trsaints2003/",
+    },
+  ];
+}
+
 export const projectService = {
   getAllProjects,
 };
 
 export const skillService = {
   getAllSkills,
-}
+};
+
+export const socialMediaService = {
+  getAllSocialLinks,
+};
