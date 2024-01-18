f7a760cf63f62ec13ccdcea9912d0be97282468e
trsaints <thiago.dev2k3@gmail.com>
2024-01-18 11:12:05 -0300

    create mockup structure

--- src/app/services/db-service.js
+++ src/app/services/db-service.js
@@ -1,5 +1,24 @@
 function getAllProjects() {
-  return ["hi mom"];
+  return [
+    {
+      id: 0,
+      title: "Project Title",
+      stack: [{ tech: "HTML" }, { tech: "CSS" }, { tech: "Javascript" }],
+      year: 2023,
+      banner: "https://picsum.photos/200/300",
+      desc: "Project Description",
+      links: [
+        {
+          name: "ver código fonte",
+          url: "https://github.com/trsaints",
+        },
+        {
+          name: "deploy",
+          url: "https://www.linkedin.com/in/trsaints2003/",
+        },
+      ],
+    },
+  ];
 }
 
 function getAllSkills() {
