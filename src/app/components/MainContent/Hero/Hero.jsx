7b8ef2776de256c63b75ed1853a2980ddab918e8
trsaints <thiago.dev2k3@gmail.com>
2024-01-20 11:11:57 -0300

    create hero section stylesheet

--- src/app/components/MainContent/Hero/Hero.jsx
+++ src/app/components/MainContent/Hero/Hero.jsx
@@ -1,13 +1,15 @@
+import "./Hero.css";
+
 export default function Hero() {
-    return (
-      <header className="hero">
-        <h1 className="hero__title">Thiago Rodrigues Santos</h1>
-  
-        <p className="hero__subtitle">web developer</p>
-  
-        <a className="hero__anchor" href="#about">
-          desça a página
-        </a>
-      </header>
-    );
-  }
+  return (
+    <header className="hero">
+      <h1 className="hero__title">Thiago Rodrigues Santos</h1>
+
+      <p className="hero__subtitle">web developer</p>
+
+      <a className="hero__anchor" href="#about">
+        desça a página
+      </a>
+    </header>
+  );
+}
