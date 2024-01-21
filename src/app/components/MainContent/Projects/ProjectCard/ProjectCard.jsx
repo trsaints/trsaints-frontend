c0176ccdcb407a03b15f1ddc2d09f6b23d1e9b37
trsaints <thiago.dev2k3@gmail.com>
2024-01-21 19:03:20 -0300

    create projects section stylesheet - II

--- src/app/components/MainContent/Projects/ProjectCard/ProjectCard.jsx
+++ src/app/components/MainContent/Projects/ProjectCard/ProjectCard.jsx
@@ -1,21 +1,25 @@
+import "./ProjectCard.css";
+
 function Root({ children }) {
-  return <article>{children}</article>;
+  return <article className="project-card">{children}</article>;
 }
 
 function Content({ project }) {
   return (
     <>
-      <h3>{project.title}</h3>
+      <h3 className="project-card__title">{project.title}</h3>
 
-      <dl>
-        <dt>stack</dt>
-        <dd>{project.stack.map((tech) => tech.tech).join(", ")}</dd>
+      <dl className="meta-data">
+        <dt className="meta-data__key">stack</dt>
+        <dd className="meta-data__value">
+          {project.stack.map((tech) => tech.tech).join(", ")}
+        </dd>
       </dl>
 
-      <dl>
-        <dt>deploy</dt>
-        <dd>
-          {project.links[1]["url"] !== "" ? project.links[1]["url"] : "none"}
+      <dl className="meta-data">
+        <dt className="meta-data__key">deploy</dt>
+        <dd className="meta-data__value">
+          {project.links[1]["url"] !== "" && project.links[1]["url"]}
         </dd>
       </dl>
     </>
