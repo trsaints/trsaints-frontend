43c0dbbe986c09edeabc63e89825bd60a706ca49
trsaints <thiago.dev2k3@gmail.com>
2024-02-12 18:52:19 -0300

    remove conditional rendering via state

--- src/app/components/App/App.jsx
+++ src/app/components/App/App.jsx
@@ -1,19 +1,15 @@
 import "./App.css";
 
-import { MainHeader } from "../MainHeader";
 import { MainContent } from "../MainContent";
 import { MainFooter } from "../MainFooter/MainFooter";
-import { useState } from "react";
+import { MainHeader } from "../MainHeader";
 
 function App() {
-  const [visible, setVisible] = useState(false);
-
   return (
     <>
       <MainHeader.Root>
         <MainHeader.Banner srcPath="/computer_logo.svg" />
-        <MainHeader.Menu onHandleClick={() => setVisible(!visible)} />
-        {visible && <MainHeader.Nav />}
+        <MainHeader.Menu />
       </MainHeader.Root>
 
       <MainContent.Root>
