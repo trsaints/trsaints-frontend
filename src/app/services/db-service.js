62ba0665b699c68620030f4ce4e16772bb2af49f
trsaints <thiago.dev2k3@gmail.com>
2024-02-13 18:42:29 -0300

    update mockup entries

--- src/app/services/db-service.js
+++ src/app/services/db-service.js
@@ -21,6 +21,27 @@ function getAllProjects() {
         },
       ],
     },
+    {
+      id: 1,
+      title: "Project Title",
+      stack: [{ tech: "HTML" }, { tech: "CSS" }, { tech: "Javascript" }],
+      year: 2023,
+      banner: {
+        src: "https://picsum.photos/200/300",
+        alt: "Project banner",
+      },
+      desc: "Project Description",
+      links: [
+        {
+          name: "ver código fonte",
+          url: "https://github.com/trsaints",
+        },
+        {
+          name: "deploy",
+          url: "https://www.linkedin.com/in/trsaints2003/",
+        },
+      ],
+    },
   ];
 }
 
@@ -29,6 +50,16 @@ function getAllSkills() {
     {
       id: 0,
       title: "HTML",
+      type: "hard skill",
+      desc: "lorem ipsum dolor sit amet",
+      banner: {
+        path: "",
+        alt: "",
+      },
+    },
+    {
+      id: 1,
+      title: "CSS",
       type: "hard skill",
       desc: "lorem ipsum dolor sit amet",
       banner: {
