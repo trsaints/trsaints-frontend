91112bebf3012f42b1fb4fe799c35607877bcd5c
trsaints <thiago.dev2k3@gmail.com>
2024-03-11 08:05:41 -0300

    fix mismatching pattern of selector

--- src/app/components/MainContent/Projects/ProjectCard/ProjectCard.jsx
+++ src/app/components/MainContent/Projects/ProjectCard/ProjectCard.jsx
@@ -19,12 +19,12 @@ function MetaStack({ stack }) {
   const mainStack = stack.map((tech) => tech.tech).join(", ");
 
   return (
-    <dl className="project__meta">
-      <dt className="project__meta__key">
+    <dl className="meta">
+      <dt className="meta__key">
         <span lang="en">stack</span>
       </dt>
 
-      <dd className="project__meta__value">{mainStack}</dd>
+      <dd className="meta__value">{mainStack}</dd>
     </dl>
   );
 }
@@ -33,10 +33,10 @@ function MetaDeploy({ links }) {
   const deployLink = links[1]["url"] !== "" && links[1]["url"];
 
   return (
-    <dl className="project__meta">
-      <dt className="project__meta__key">deploy</dt>
+    <dl className="meta">
+      <dt className="meta__key">deploy</dt>
 
-      <dd className="project__meta__value">{deployLink}</dd>
+      <dd className="meta__value">{deployLink}</dd>
     </dl>
   );
 }
