61341dda9680285c5606f011f0dc62b84a77543a
trsaints <thiago.dev2k3@gmail.com>
2024-03-12 09:57:49 -0300

    add basic transition for interactive elements

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./app/components/App/App";
 import "./reset.css";
 import "./index.css";
 import "./interactions.css";
+import "./animations.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
