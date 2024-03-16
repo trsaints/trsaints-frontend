46f373fdf736bfe6ae7a86ab088270fcf51042f5
trsaints <thiago.dev2k3@gmail.com>
2024-03-16 00:13:25 -0300

    add functions for abstracting  useEffect steps

--- src/app/services/intersection-service.js
+++ src/app/services/intersection-service.js
@@ -1,43 +1,60 @@
 import { useEffect, useRef } from "react";
 
-// Cria um contexto global para o observador
-const observerContext = new Map();
+const globalObserverContext = new Map();
 
-// Cria um Hook personalizado para usar o observador
+function hasThreshold(threshold) {
+  return globalObserverContext.has(threshold);
+}
+
+function addObserverToContext(threshold, animationName) {
+  const observer = getIntersectionObserver(threshold, animationName);
+  globalObserverContext.set(threshold, observer);
+}
+
+function getIntersectionObserver(threshold, animationName) {
+  const observer = new IntersectionObserver(
+    (entries) => setObserverEntries(entries, observer, animationName),
+    getOptions(threshold)
+  );
+
+  return observer;
+}
+
+function setObserverEntries(entries, observer, animationName) {
+  entries.forEach((entry) =>
+    updateEntryClassList(entry, observer, animationName)
+  );
+}
+
+function updateEntryClassList(entry, observer, animationName) {
+  if (entry.isIntersecting) {
+    entry.target.classList.add(animationName);
+    observer.unobserve(entry.target);
+  }
+}
+
+function getOptions(threshold) {
+  return {
+    root: null,
+    rootMargin: "0px",
+    threshold: threshold,
+  };
+}
+
 function useIntersectionObserver(threshold, animationName) {
   const ref = useRef();
 
   useEffect(() => {
-    if (!observerContext.has(threshold)) {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              entry.target.classList.add(animationName);
-              observer.unobserve(entry.target);
-            }
-          });
-        },
-        {
-          root: null,
-          rootMargin: "0px",
-          threshold: threshold,
-        }
-      );
-      observerContext.set(threshold, observer);
-    }
+    if (!hasThreshold(threshold))
+      addObserverToContext(threshold, animationName);
 
-    const observer = observerContext.get(threshold);
+    const observer = globalObserverContext.get(threshold),
+      isCurrentRef = ref.current !== undefined;
 
-    if (ref.current) {
-      observer.observe(ref.current);
-    }
+    if (!isCurrentRef) return;
 
-    return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
-      }
-    };
+    observer.observe(ref.current);
+    return () => observer.unobserve(ref.current);
   }, [threshold, animationName]);
 
   return ref;
