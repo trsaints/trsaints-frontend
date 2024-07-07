f6f6e846b750f895a1cfc7951dcec769e18d8a20
trsaints <thiago.dev2k3@gmail.com>
2024-07-07 18:02:02 -0300

    refactor: divide ProjectCard.tsx into new components

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,49 +1,21 @@
 import './ProjectCard.css'
+import {IProjectCard} from './IProjectCard'
+import {MetaStack} from '../MetaStack'
+import {MetaDeploy} from '../MetaDeploy'
 
-function Root({children}) {
-    return <article className='project-card'>{children}</article>
-}
+function ProjectCard(props: IProjectCard) {
+    const {stack, project} = props
 
-function Content({project}) {
     return (
-        <>
+        <article className='project-card'>
             <h3 className='project-card__title'>{project.title}</h3>
 
-            <MetaStack stack={project.stack}/>
-            <MetaDeploy links={project.links}/>
+            <MetaStack stack={stack}/>
+            <MetaDeploy deployUrl={project.deployUrl ?? ''}/>
 
             <button className='sr-only' type='button'>ver projeto: {project.title}</button>
-        </>
+        </article>
     )
 }
 
-function MetaStack({stack}) {
-    const mainStack = stack.map((tech) => tech.tech).join(', ')
-
-    return (
-        <dl className='meta'>
-            <dt className='meta__key'>
-                <span lang='en'>stack</span>
-            </dt>
-
-            <dd className='meta__value'>{mainStack}</dd>
-        </dl>
-    )
-}
-
-function MetaDeploy({links}) {
-    const deployLink = links[1]['url'] !== '' && links[1]['url']
-
-    return (
-        <dl className='meta'>
-            <dt className='meta__key'>deploy</dt>
-
-            <dd className='meta__value'>{deployLink}</dd>
-        </dl>
-    )
-}
-
-export const ProjectCard = {
-    Root,
-    Content,
-}
+export {ProjectCard}
