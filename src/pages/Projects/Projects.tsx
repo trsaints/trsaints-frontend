a29d89e4d977ecd457a0657bb19fe63b774613cd
trsaints <thiago.dev2k3@gmail.com>
2024-07-07 15:39:25 -0300

    remove: temporarily disable intersectionService usages

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,5 +1,4 @@
 import {faArrowDown} from '@fortawesome/free-solid-svg-icons'
-import {intersectionService} from '../../services'
 
 import {Modal, ProjectCard, ProjectFilter, ProjectModal} from '../../components'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
@@ -15,11 +14,9 @@ function Root({children, onHandleKeyDown}) {
 }
 
 function Header({onHandleClick}) {
-    const ref = intersectionService.useIntersectionObserver(0.5, 'typed-out')
-
     return (
         <header className='projects__header'>
-            <h2 className='projects__title' ref={ref}>
+            <h2 className='projects__title' >
                 meus projetos
             </h2>
 
