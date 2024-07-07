757a15033700db471d49e3691ab21af2b258a8d6
trsaints <thiago.dev2k3@gmail.com>
2024-07-06 22:36:18 -0300

    refactor: reduce redundancy on components/pages structures

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,23 +7,9 @@ function App() {
   return (
     <>
       <ScrollTop />
-
-      <MainHeader.Root>
-        <MainHeader.Banner srcPath="/computer_logo.svg" />
-        <MainHeader.Menu />
-      </MainHeader.Root>
-
-      <MainContent.Root>
-        <MainContent.HeroSection />
-        <MainContent.AboutSection />
-        <MainContent.SkillsSection />
-        <MainContent.ProjectsSection />
-        <MainContent.ContactSection />
-      </MainContent.Root>
-
-      <MainFooter.Root>
-        <MainFooter.Content />
-      </MainFooter.Root>
+      <MainHeader srcPath='/computer_logo.svg' />
+      <MainContent />
+      <MainFooter />
     </>
   );
 }
