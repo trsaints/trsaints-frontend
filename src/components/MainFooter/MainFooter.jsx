3f8c0ecf9c0a7ab008b8a2aaef5c07a2b1118166
trsaints <thiago.dev2k3@gmail.com>
2024-07-06 22:36:18 -0300

    refactor: reduce redundancy on components/pages structures

--- src/components/MainFooter/MainFooter.jsx
+++ src/components/MainFooter/MainFooter.jsx
@@ -1,38 +1,33 @@
-import "./MainFooter.css";
+import "./MainFooter.css"
 
-function Root({ children }) {
-  return <footer className="footer">{children}</footer>;
+function MainFooter() {
+    const currentYear = new Date().getFullYear()
+
+    return (
+        <footer className="footer">
+            <article className="footer__content">
+                <h2 className="footer__title">
+                    thiago rodrigues santos - desenvolvedor <span lang="en">front-end</span>
+                </h2>
+
+                <time className="footer__time" dateTime={currentYear}>
+                    {currentYear}
+                </time>
+
+                <p className="footer__desc">
+                    feito por{" "}
+                    <a
+                        lang="en"
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://github.com/trsaints"
+                    >
+                        @trsaints
+                    </a>
+                </p>
+            </article>
+        </footer>
+    )
 }
 
-function Content() {
-  const currentYear = new Date().getFullYear();
-
-  return (
-    <article className="footer__content">
-      <h2 className="footer__title">
-        thiago rodrigues santos - desenvolvedor <span lang="en">front-end</span>
-      </h2>
-
-      <time className="footer__time" dateTime={currentYear}>
-        {currentYear}
-      </time>
-
-      <p className="footer__desc">
-        feito por{" "}
-        <a
-          lang="en"
-          rel="noopener"
-          target="_blank"
-          href="https://github.com/trsaints"
-        >
-          @trsaints
-        </a>
-      </p>
-    </article>
-  );
-}
-
-export const MainFooter = {
-  Root,
-  Content,
-};
+export {MainFooter}
--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,22 +1,15 @@
-import { HeaderMenu } from "../HeaderMenu";
+import {HeaderMenu} from "../HeaderMenu"
 
-import "./MainHeader.css";
+import "./MainHeader.css"
 
-function Root({ children }) {
-  return <header className="header">{children}</header>;
+function MainHeader({srcPath}) {
+    return (
+        <header className="header">
+            <img className="header__banner" src={srcPath} alt=""/>
+
+            <HeaderMenu />
+        </header>
+    )
 }
 
-function Banner({ srcPath }) {
-  return <img className="header__banner" src={srcPath} alt="" />;
-}
-
-function Menu() {
-  return (
-    <HeaderMenu.Root>
-      <HeaderMenu.Button />
-      <HeaderMenu.Nav />
-    </HeaderMenu.Root>
-  );
-}
-
-export const MainHeader = { Root, Banner, Menu };
+export {MainHeader}
