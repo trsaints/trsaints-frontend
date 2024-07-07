a90bba44949b9d0f84592c63ce411ea4ca3b9e4a
trsaints <thiago.dev2k3@gmail.com>
2024-07-07 18:02:02 -0300

    refactor: divide ProjectCard.tsx into new components

---
+++ src/components/MetaDeploy/MetaDeploy.tsx
@@ -0,0 +1,17 @@
+import {IMetaDeploy} from './IMetaDeploy'
+import './MetaDeploy.css'
+
+function MetaDeploy(props: IMetaDeploy) {
+    const {deployUrl} = props
+    const hasDeploy   = deployUrl !== ''
+
+    return hasDeploy && (
+        <dl className='meta'>
+            <dt className='meta__key'>deploy</dt>
+
+            <dd className='meta__value'>{deployUrl}</dd>
+        </dl>
+    )
+}
+
+export {MetaDeploy}
---
+++ src/components/MetaStack/MetaStack.tsx
@@ -0,0 +1,21 @@
+import {ComponentProps} from 'react'
+import './MetaStack.css'
+interface IMetaStack extends ComponentProps<'dl'> {
+    stack: string
+}
+
+function MetaStack(props: IMetaStack) {
+    const {stack} = props
+
+    return (
+        <dl className='meta'>
+            <dt className='meta__key'>
+                <span lang='en'>stack</span>
+            </dt>
+
+            <dd className='meta__value'>{stack}</dd>
+        </dl>
+    )
+}
+
+export {MetaStack}
