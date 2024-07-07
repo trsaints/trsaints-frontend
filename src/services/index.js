c61c70a82db674920362ccd99cf789d8956660fc
trsaints <thiago.dev2k3@gmail.com>
2024-07-07 09:53:18 -0300

    refactor: separate distinct services into new files

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
-export * from './db-service.js'
+export * from './social-media-service.js'
 export * from './validation-service.js'
 export * from './intersection-service.js'
+export * from './project-service.js'
+export * from './skill-service.js'
