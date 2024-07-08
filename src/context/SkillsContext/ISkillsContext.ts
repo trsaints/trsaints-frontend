a3c4857d3a307d5971244734b2bb266d364cd927
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-07-08 11:26:06 -0300

    Revert "feat: create SkillsContext for isolating Skills page event listeners"
    This reverts commit ed3fd102dd3c2cfbc9d05f67ff5df904d576a169.

--- src/context/SkillsContext/ISkillsContext.ts
+++
@@ -1,8 +0,0 @@
-import {KeyboardEventHandler, MouseEventHandler} from 'react'
-
-export interface ISkillsContext {
-    selectSkill: MouseEventHandler<HTMLUListElement>
-    hideSkill: MouseEventHandler<HTMLButtonElement>
-    closeOnEscape: KeyboardEventHandler<HTMLElement>
-    loadSkills: MouseEventHandler<HTMLAnchorElement>
-}
