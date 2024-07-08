cfd10a403c6d4dd595c3f5ac99be0c5a499bc95a
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-07-08 11:26:06 -0300

    Revert "refactor: extract AboutHeader component to separate file"
    This reverts commit fa5b7f09622bc2b8b91f5a9b3b180c298e0fef53.

--- src/components/AboutHeader/IAboutHeader.ts
+++
@@ -1,6 +0,0 @@
-import {ComponentProps} from 'react'
-import {SocialLink} from '../../types/SocialLink'
-
-export interface IAboutHeader extends ComponentProps<'header'> {
-    links: SocialLink[]
-}
--- src/pages/About/IAbout.ts
+++
@@ -1,7 +0,0 @@
-import {ComponentProps} from 'react'
-import {SocialLink} from '../../types/SocialLink'
-
-export interface IAbout extends ComponentProps<'article'> {
-    links: SocialLink[]
-    srcPath: string
-}
