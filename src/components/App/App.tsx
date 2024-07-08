51a71801fab87f0c3b3a2250c63b9cfccc355d5a
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-07-08 11:26:06 -0300

    Revert "feat: migrate local state management to context usage"
    This reverts commit 0a204b227025e0a88a006a0dc0f13a265e440fdd.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,18 +2,13 @@ import {Footer, Header, ScrollTop} from '../../components'
 import {MainContent} from '../../layouts'
 
 import './App.css'
-import {MainContextProvider} from '../../context/providers/MainContextProvider'
 
 function App() {
     return (
         <>
             <ScrollTop/>
             <Header srcPath='/computer_logo.svg'/>
-            
-            <MainContextProvider>
-                <MainContent/>
-            </MainContextProvider>
-            
+            <MainContent/>
             <Footer/>
         </>
     )
