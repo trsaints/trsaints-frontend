dd39ce462dcd1a7614fae262d437aacd5b5b1ede
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-07-08 11:26:06 -0300

    Revert "fix: extract SkillModal subcomponents into separate files"
    This reverts commit b2ab3c6baa1dc42e3849b74947d468cdd185f9ce.

--- src/components/SkillModal/SkillModal.tsx
+++ src/components/SkillModal/SkillModal.tsx
@@ -1,16 +1,60 @@
-import {SkillModalHeader} from '../SkillModalHeader'
-import {SkillModalContent} from '../SkillModalContent'
-
-import {ISkillModal} from './ISkillModal'
 import './SkillModal.css'
+import {ComponentProps, PropsWithChildren} from 'react'
+import {Skill} from '../../models'
+
+interface IHeader extends PropsWithChildren {
+    skill: Skill
+}
+
+function Header(props: IHeader) {
+    const {skill} = props
+    
+    return (
+        <header className='skill__header'>
+            <h3 className='skill__title'>{skill.title}</h3>
+
+            <dl className='skill-meta'>
+                <dt className='sr-only'>tipo:</dt>
+
+                <dd className='skill-meta__value' lang='en'>
+                    {skill.category}
+                </dd>
+            </dl>
+        </header>
+    )
+}
+
+interface IContent extends PropsWithChildren {
+    skill: Skill    
+}
+
+function Content(props: IContent) {
+    const {skill} = props
+    
+    return (
+        <>
+            <p className='skill__desc'>{skill.description}</p>
+
+            <figure className='skill__banner'>
+                <figcaption>{skill.title}</figcaption>
+
+                <img className='skill__img' src={skill.bannerUrl} alt={skill.bannerAlt}/>
+            </figure>
+        </>
+    )
+}
+
+interface ISkillModal extends ComponentProps<'article'> {
+    skill: Skill
+}
 
 function SkillModal(props: ISkillModal) {
     const {skill} = props
-
+    
     return (
         <article className='skill'>
-            <SkillModalHeader {...skill}/>
-            <SkillModalContent {...skill}/>
+            <Header skill={skill}/>
+            <Content skill={skill}/>
         </article>)
 }
 
