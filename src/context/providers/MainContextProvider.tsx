4c7de251f901310fb589f90cb3638f1fe5342c4c
trsaints <thiago.dev2k3@gmail.com>
2024-07-08 07:54:50 -0300

    update: change initial state

--- src/context/providers/MainContextProvider.tsx
+++ src/context/providers/MainContextProvider.tsx
@@ -2,17 +2,16 @@ import {MainContext} from '../MainContext'
 import {useState} from 'react'
 import {IMainContextProvider} from './IMainContextProvider'
 import {Project, Skill} from '../../models'
-import {projectService, skillService} from '../../services'
 import {IMainContext} from '../IMainContext'
 
 function MainContextProvider(props: IMainContextProvider) {
     const {children} = props
 
-    const [projects, setProjects] = useState<Project[]>(projectService.getPlaceholderProjects())
+    const [projects, setProjects] = useState<Project[]>([])
     const [projectId, setProjectId] = useState<number>(-1)
     
     const [skillId, setSkillId] = useState<number>(-1)
-    const [skills, setSkills] = useState<Skill[]>(skillService.getPlaceholderSkills())
+    const [skills, setSkills] = useState<Skill[]>([])
     
     const [search, setSearch] = useState<string>('')
 
