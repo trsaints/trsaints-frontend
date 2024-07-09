223cb15865a818368bfa7e536595c14fd4c8e21f
trsaints <thiago.dev2k3@gmail.com>
2024-07-08 21:41:01 -0300

    feat: create FormField component

---
+++ src/components/FormField/FormField.tsx
@@ -0,0 +1,41 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+import {IFormField} from './IFormField'
+import './FormField.css'
+import React, {DetailedHTMLProps, ReactElement} from 'react'
+function FormField(props: IFormField) {
+    const {
+              icon
+              , label
+              , id
+              , iconPosition
+              , placeholder
+              , inputType
+          }            = props
+    const iconSelector = `${iconPosition}-icon`
+
+    let inputElement: ReactElement
+
+    if (inputType)
+        inputElement = <input className='form-field__input'
+                              type={inputType}
+                              id={id}
+                              placeholder={placeholder}/>
+    else
+        inputElement = <textarea className='form-field__textarea'
+                                 id={id}
+                                 placeholder={placeholder}/>
+
+    return (
+        <div className='form-field'>
+            <label className='form-field__label' htmlFor={id}>
+                <FontAwesomeIcon className={iconSelector} icon={icon}/>
+                {label}
+            </label>
+
+            {inputElement}
+        </div>
+    )
+}
+
+export {FormField}
