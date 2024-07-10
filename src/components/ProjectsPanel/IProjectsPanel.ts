1fc59bbabf47536d05a01171f138a230d7c74b82
trsaints <thiago.dev2k3@gmail.com>
2024-07-10 10:12:48 -0300

    remove: remove custom modal content temporarily

--- src/components/ProjectsPanel/IProjectsPanel.ts
+++
@@ -1,5 +0,0 @@
-import {ComponentProps} from 'react'
-import {Project} from '../../models'
-export interface IProjectsPanel extends ComponentProps<'dialog'> {
-    project?: Project
-}
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -2,31 +2,23 @@ import {projectService} from '../../services'
 
 import {useContext} from 'react'
 import {ProjectsContextProvider} from '../../context/providers/ProjectsContextProvider'
-import {ProjectsContext} from '../../context/ProjectsContext'
 import {MainContext} from '../../context/MainContext'
-
-import {ProjectsPanel} from '../../components/ProjectsPanel'
 import {ProjectsContent} from '../../components/ProjectsContent'
 import {ProjectsHeader} from '../../components/ProjectsHeader'
 
 import './Projects.css'
 
 function Projects() {
-    const {projects}  = useContext(MainContext)
-    const {projectId} = useContext(MainContext)
-
-    const {closeOnEscape} = useContext(ProjectsContext)
-    const {search, sort}  = useContext(MainContext)
+    const {projects, projectId, search} = useContext(MainContext)
 
     const projectsFound   = projectService.filterProjects(projects, search)
     const selectedProject = projectService.getProjectById(projects, projectId)
 
     return (
         <ProjectsContextProvider>
-            <article className='projects' id='projects' onKeyDown={closeOnEscape}>
+            <article className='projects' id='projects'>
                 <ProjectsHeader/>
                 <ProjectsContent baseProjectsLength={projects.length} projects={projectsFound}/>
-                <ProjectsPanel project={selectedProject}/>
             </article>
         </ProjectsContextProvider>
     )
