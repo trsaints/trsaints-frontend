f01004f83dd511ae4330800ca574cfb23324be5f
trsaints <thiago.dev2k3@gmail.com>
2024-07-09 21:43:26 -0300

    refactor: migrate MainContext scope for handling Modal behavior globally

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,23 @@
-import {Footer, Header, ScrollTop} from '../../components'
+import {useContext, useEffect} from 'react'
+import {MainContext} from '../../context/MainContext'
 
+import {Footer, Header, ScrollTop} from '../../components'
 import {Default} from '../../layouts'
 
 import './App.css'
 
 function App() {
+    const {setIsModalOpen} = useContext(MainContext)
+
+    useEffect(() => {
+        const closeOnEscape = (event: KeyboardEvent) =>
+            event.key === 'Escape' && setIsModalOpen(false)
+
+        document.addEventListener('keydown', closeOnEscape)
+
+        return () => document.removeEventListener('keydown', closeOnEscape)
+    }, [setIsModalOpen])
+    
     return (
         <>
             <ScrollTop/>
--- src/layouts/Default/Default.tsx
+++ src/layouts/Default/Default.tsx
@@ -1,21 +1,20 @@
 import {socialMediaService,} from '../../services'
 
 import {About, Contact, Hero, Projects, Skills} from '../../pages'
-import {MainContextProvider} from '../../context/providers'
+import {Modal} from '../../components'
 
 function Default() {
     const links = socialMediaService.getAllSocialLinks()
 
     return (
-        <MainContextProvider>
-            <main className='main'>
-                <Hero/>
-                <About links={links} srcPath={''}/>
-                <Skills/>
-                <Projects/>
-                <Contact/>
-            </main>
-        </MainContextProvider>
+        <main className='main'>
+            <Hero/>
+            <About links={links} srcPath={''}/>
+            <Skills/>
+            <Projects/>
+            <Modal/>
+            <Contact/>
+        </main>
     )
 }
 
