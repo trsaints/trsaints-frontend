940b9454f3f82234ebc677d4d6f3168ca92543fd
trsaints <thiago.dev2k3@gmail.com>
2024-07-11 19:41:30 -0300

    refactor: remove redundancies and centralize event management at `Skills` page

---
+++ src/components/SkillsList/SkillsList.tsx
@@ -0,0 +1,24 @@
+import './SkillsList.css'
+import {SkillCard} from '../SkillCard'
+import {ISkillsList} from './ISkillsList'
+
+function SkillsList(props: ISkillsList) {
+    const {skills} = props
+
+    const skillCards = skills.map((skill) => (
+        <li key={skill?.uuid} data-id={skill?.uuid}>
+            <SkillCard.Root>
+                <SkillCard.Content skill={skill}/>
+            </SkillCard.Root>
+        </li>
+    ))
+
+    return (skills.length > 0)
+        && (
+            <ul className='skills__list window-frame' id='skills-list'>
+                {skillCards}
+            </ul>
+        )
+}
+
+export {SkillsList}
