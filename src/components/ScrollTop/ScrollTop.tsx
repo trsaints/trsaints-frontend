b3b3198d3caca36f37a8ab5e8f729037042254e7
trsaints <thiago.dev2k3@gmail.com>
2024-07-11 22:24:46 -0300

    refactor: convert to normal function component

--- src/components/ScrollTop/ScrollTop.tsx
+++ src/components/ScrollTop/ScrollTop.tsx
@@ -4,7 +4,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 import './ScrollTop.css'
 
-const ScrollTop = () => {
+function ScrollTop() {
     return (
         <a className='scroll-top' href='#' tabIndex={0}>
             <span className='sr-only'>Voltar ao início</span>
